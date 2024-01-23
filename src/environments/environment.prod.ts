ebd6f4b48afe8e735de758825084c434c4941ce1
andrupax94 <52213160+andrupax94@users.noreply.github.com>
2024-01-23 17:36:33 +0000

    ghjghj

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: '<appApiLaravel.com>',
+    apiUrl: '',
+    back: 'https://laravel.andreseduardo.es/api/',
     public: '/wp-content/plugins/lacholaplugin/',
-    baseRef:'/wp-admin/admin.php?page=subdominio-plugin'
-  };
+    baseRef: '/wp-admin/admin.php?page=subdominio-plugin'
+};
